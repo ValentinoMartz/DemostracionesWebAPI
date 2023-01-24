2fcc467b57abdc9a77600a8bba06426a8dfac16b
ValentinoMartz <valentinomartinez55@gmail.com>
2023-01-24 14:53:42 -0300

    Extras terminados

--- WebApiPubs/Controllers/StoreController.cs
+++ WebApiPubs/Controllers/StoreController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Collections.Generic;
 using System.Linq;
 using WebApiPubs.Models;
@@ -38,12 +39,81 @@ namespace WebApiPubs.Controllers
         }
 
         //Put
+        [HttpPut("{id}")]
+        public ActionResult<Store> Put(string id, [FromBody] Store store)
+        {
+            if (id != store.StorId)
+            {
+                return BadRequest();
+            }
+            context.Entry(store).State = EntityState.Modified;
+            context.SaveChanges();
+            return Ok();
+        }
+
         //Delete
+        [HttpDelete("{id}")]
+        public ActionResult<Store> Delete(string id)
+        {
+            var store = (from c in context.Stores
+                         where c.StorId == id
+                         select c).SingleOrDefault();
+
+            if (store == null)
+            {
+                return NotFound();
+            }
+            context.Stores.Remove(store);
+            context.SaveChanges();
+            return store;
+        }
+
 
         //Adicionales
         //GetbyId
+        [HttpGet("{id}")]
+        public ActionResult<Store> GetById(string ID)
+        {
+            Store store = (from e in context.Stores
+                           where e.StorId == ID
+                           select e).SingleOrDefault();
+            return store;
+        }
+
         //GetbyName
+        [HttpGet("name/{name}")]
+        public ActionResult<Store> GetByName(string name)
+        {
+            Store store = (from c in context.Stores
+                         where c.StorName == name
+                         select c).SingleOrDefault();
+
+            if (store == null)
+            {
+                return NotFound();
+            }
+            return store;
+        }
         //GetbyZip
+        [HttpGet("zip/{zip}")]
+        public ActionResult<IEnumerable<Store>> GetByZip(string zip)
+        {
+
+            List<Store> stores = (from e in context.Stores
+                                  where e.Zip == zip
+                                  select e).ToList();
+            return stores;
+        }
+
         //GetbyCityState
+        [HttpGet("city/{city}")]
+        public ActionResult<IEnumerable<Store>> GetByCity(string city)
+        {
+
+            List<Store> stores = (from e in context.Stores
+                                  where e.City == city
+                                  select e).ToList();
+            return stores;
+        }
     }
 }
